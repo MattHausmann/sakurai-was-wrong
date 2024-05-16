13524dba769e92615ca8b6673f7cda608d178266
Mart <themcman@gmail.com>
2024-05-16 02:59:27 -0500

    MatchupContainer uses winsDisplay

--- src/MatchupContainer.js
+++ src/MatchupContainer.js
@@ -1,81 +1,47 @@
 // MatchupContainer.js
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 import { PieChart } from "@mui/x-charts/PieChart";
 
 import LabeledCharacterPortrait from "./CharacterPortrait";
-import { MatchupNavigator, first, prev, next, last, randomMatchup, getWins } from "./MatchupNavigator.js";
+import { MatchupNavigator } from "./MatchupNavigator.js";
 import MatchupRecordDisplay from "./MatchupRecordDisplay";
+import QuizModeSlider from "./QuizModeSlider";
 
 import "./MatchupContainer.css";
 
 let possibleMatchups = {};
 const MATCHUP_THRESHOLD = 200;
 
-//first, if the videogameId changes,you need to create a new matchup with the new videogameId
-//then if the videogameId, leftCharacter, or rightCharacter changes,
 const MatchupContainer = () => {
-	const { matchup, quizMode } = useSelector((state) => state);
-	const dispatch = useDispatch();
-
-  let {videogameId, left, right} = matchup;
-
-  const [leftCharacterName, setLeftCharacterName] = useState(left);
-  const [rightCharacterName, setRightCharacterName] = useState(right);
-
-  let [winsL, winsR] = getWins(matchup);
-
-  //winsL and winsR are the correct answer
-  //leftWins and rightWins are what's displayed
-  const [leftWins, setLeftWins] = useState(winsL);
-  const [rightWins, setRightWins] = useState(winsR);
-
-  const [sliderValue, setSliderValue] = useState(leftWins);
-
-
-  useEffect(() => {
-    let [winsL, winsR] = getWins(matchup);
-    let newLeftWins = winsL;
-    let newRightWins = winsR;
-    if (quizMode) {
-      let halfWins = Math.ceil((winsL + winsR) / 2);
-      newLeftWins = halfWins;
-      newRightWins = winsL + winsR - newLeftWins;
-    }
-    setLeftWins(newLeftWins);
-    setRightWins(newRightWins);
-
-    setSliderValue(leftWins);
-  }, [matchup]);
-
+  const { matchup, quizMode, winsDisplay } = useSelector((state) => state);
+  let { left, right } = matchup;
 
   return (
     <div className="matchup-container">
       <div className="top-row">
-          <LabeledCharacterPortrait side="left" />
+        <LabeledCharacterPortrait side="left" />
         <div className="matchup-container-center">
           <MatchupRecordDisplay
-            leftWins={winsL}
-            rightWins={winsR}
+            leftWins={winsDisplay[0]}
+            rightWins={winsDisplay[1]}
           />
           <div className="matchup-graphs">
             <div className="pie-chart-container">
               <PieChart
                 slotProps={{ legend: { hidden: true } }}
-                // legend: { classes: ["pie-chart-legend"] } }}
-                // hidden: true } }}
                 series={[
                   {
                     data: [
                       {
                         id: 0,
-                        value: winsR,
+                        value: winsDisplay[1],
                         label: right,
                         color: "#cc76a1",
                       },
                       {
                         id: 1,
-                        value: winsL,
+                        value: winsDisplay[0],
                         label: left,
                         color: "#87b38d",
                       },
@@ -86,21 +52,16 @@ const MatchupContainer = () => {
                 height={200}
               />
             </div>
-            {quizMode && (
-              <div style={{ width: "100%", display: "flex" }}>
-                TODO: quizMode slider here
-              </div>
-            )}
+            {quizMode && <QuizModeSlider winsDisplay={winsDisplay} />}
           </div>
         </div>
-          <LabeledCharacterPortrait side="right" />
+        <LabeledCharacterPortrait side="right" />
       </div>
-	  <div class="bottom-row">
-		<MatchupNavigator />
-	  </div>
+      <div class="bottom-row">
+        <MatchupNavigator />
+      </div>
     </div>
   );
 };
 
 export default MatchupContainer;
-
