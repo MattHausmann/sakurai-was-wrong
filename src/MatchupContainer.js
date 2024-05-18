ad997238353178662e294b528974ce9143e3b202
Matt Hausmann <matthew.p.hausmann@gmail.com>
2024-05-17 19:12:27 -0500

    backend for winnerOnLeft, added prop for lockLeftCharacter

--- src/MatchupContainer.js
+++ src/MatchupContainer.js
@@ -1,5 +1,5 @@
 // MatchupContainer.js
-import React from "react";
+import { useState, React }from "react";
 import { useSelector } from "react-redux";
 import { PieChart } from "@mui/x-charts/PieChart";
 
@@ -10,57 +10,62 @@ import QuizModeSlider from "./QuizModeSlider";
 
 import "./MatchupContainer.css";
 
-let possibleMatchups = {};
-const MATCHUP_THRESHOLD = 200;
 
 const MatchupContainer = () => {
   const { matchup, quizMode, winsDisplay } = useSelector((state) => state);
   let { left, right } = matchup;
+  
+  console.log('in matchupcontainer');
+  
+  const [winnerOnLeft, setWinnerOnLeft] = useState(false);
+  const [lockLeftCharacter, setLockLeftCharacter] = useState(false);
+  
 
   return (
-    <div className="matchup-container">
-      <div className="top-row">
-        <LabeledCharacterPortrait side="left" />
-        <div className="matchup-container-center">
-          <MatchupRecordDisplay
-            leftWins={winsDisplay[0]}
-            rightWins={winsDisplay[1]}
-          />
-          <div className="matchup-graphs">
-            <div className="pie-chart-container">
-              <PieChart
-                slotProps={{ legend: { hidden: true } }}
-                series={[
-                  {
-                    data: [
-                      {
-                        id: 0,
-                        value: winsDisplay[1],
-                        label: right,
-                        color: "#cc76a1",
-                      },
-                      {
-                        id: 1,
-                        value: winsDisplay[0],
-                        label: left,
-                        color: "#87b38d",
-                      },
-                    ],
-                  },
-                ]}
-                width={200}
-                height={200}
-              />
-            </div>
-            {quizMode && <QuizModeSlider winsDisplay={winsDisplay} />}
-          </div>
-        </div>
-        <LabeledCharacterPortrait side="right" />
-      </div>
-      <div class="bottom-row">
-        <MatchupNavigator />
-      </div>
-    </div>
+	<div className="matchup-container">
+		<div className="top-row">
+			<LabeledCharacterPortrait side={winnerOnLeft?"left":"right"} />
+			<div className="matchup-container-center">
+				<MatchupRecordDisplay
+					leftWins={winsDisplay[winnerOnLeft?0:1]}
+					rightWins={winsDisplay[winnerOnLeft?1:0]}
+				/>
+				<div className="matchup-graphs">
+					<div className="pie-chart-container">
+						<PieChart
+							slotProps={{ legend: { hidden: true } }}
+							series={[
+							{
+								data: [
+								{
+									id: 0,
+									value: winsDisplay[winnerOnLeft?1:0],
+									label: right,
+									color: "#cc76a1",
+								},
+
+								{
+									id: 1,
+									value: winsDisplay[winnerOnLeft?0:1],
+									label: left,
+									color: "#87b38d",
+								},
+								],
+							},
+							]}
+							width={200}
+							height={200}
+						/>
+					</div>
+				{quizMode && <QuizModeSlider winsDisplay={winsDisplay} />}
+				</div>
+			</div>
+			<LabeledCharacterPortrait side={winnerOnLeft?"right":"left"} />
+		</div>
+		<div class="bottom-row">
+			<MatchupNavigator />
+		</div>
+	</div>
   );
 };
 
