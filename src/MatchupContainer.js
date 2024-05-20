6179a77b5ce597d1fafa42ed386254d45cbac28b
Mart <themcman@gmail.com>
2024-05-19 22:54:18 -0500

    guess animation

--- src/MatchupContainer.js
+++ src/MatchupContainer.js
@@ -1,71 +1,50 @@
 // MatchupContainer.js
-import { useState, React }from "react";
+import { useState, React } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { PieChart } from "@mui/x-charts/PieChart";
 
 import LabeledCharacterPortrait from "./CharacterPortrait";
 import { MatchupNavigator } from "./MatchupNavigator.js";
 import MatchupRecordDisplay from "./MatchupRecordDisplay";
+import PieChartComponent from "./PieChartComponent";
 import QuizModeSlider from "./QuizModeSlider";
 
 import "./MatchupContainer.css";
 
-
 const MatchupContainer = () => {
-	const dispatch = useDispatch();
-	const { matchup, quizMode, winsDisplay, lockLeft } = useSelector((state) => state);
-	let { left, right } = matchup;
+  const dispatch = useDispatch();
+  const { matchup, quizMode, winsDisplay, lockLeft } = useSelector(
+    (state) => state
+  );
+  let { left, right } = matchup;
 
-
-	return (
-		<div className="matchup-container">
-			<div className="top-row">
-				<LabeledCharacterPortrait 
-					lockSwitch 
-					side={"left"} 
-					onClick = {() => {dispatch({type:"toggleLockLeft"})} }
-				/>
-				<div className="matchup-container-center">
-					<MatchupRecordDisplay
-						leftWins={winsDisplay[0]}
-						rightWins={winsDisplay[1]}
-					/>
-					<div className="matchup-graphs">
-						<div className="pie-chart-container">
-							<PieChart
-								slotProps={{ legend: { hidden: true } }}
-								series={[
-								{
-									data: [
-									{
-										id: 0,
-										value: winsDisplay[1],
-										label: right,
-										color: "#cc76a1",
-									},
-
-									{
-										id: 1,
-										value: winsDisplay[0],
-										label: left,
-										color: "#87b38d",
-									},
-									],
-								},
-								]}
-								width={200}
-								height={200}
-							/>
-						</div>
-					{quizMode && <QuizModeSlider winsDisplay={winsDisplay} />}
-					</div>
-				</div>
-				<LabeledCharacterPortrait side={"right"} />
-			</div>
-			<div class="bottom-row">
-				<MatchupNavigator lockLeft={lockLeft}/>
-			</div>
-		</div>
+  return (
+    <div className="matchup-container">
+      <div className="top-row">
+        <LabeledCharacterPortrait
+          lockSwitch
+          side={"left"}
+          onClick={() => {
+            dispatch({ type: "toggleLockLeft" });
+          }}
+        />
+        <div className="matchup-container-center">
+          <MatchupRecordDisplay
+            leftWins={winsDisplay[0]}
+            rightWins={winsDisplay[1]}
+          />
+          <div className="matchup-graphs">
+            <div className="pie-chart-container">
+              <PieChartComponent />
+            </div>
+            {quizMode && <QuizModeSlider winsDisplay={winsDisplay} />}
+          </div>
+        </div>
+        <LabeledCharacterPortrait side={"right"} />
+      </div>
+      <div class="bottom-row">
+        <MatchupNavigator lockLeft={lockLeft} />
+      </div>
+    </div>
   );
 };
 
--- src/store.js
+++ src/store.js
@@ -3,17 +3,18 @@ import { createStore } from "redux";
 import { randomMatchup } from "./MatchupNavigator";
 
 let initialState = {
-	bestScores: {},
-	currentIndex: 1,
-	guessedMatchups: {},
-	minimumGames: 1,
-	orderBy: "Left Win %",
-	quizMode: false,
-	quizResults: [],
-	selectedGames: [],
-	seenMatchups: {},
-	winsDisplay: [0, 0],
-	lockLeft: false,
+  bestScores: {},
+  currentIndex: 1,
+  guessedMatchups: {},
+  matchup: {},
+  minimumGames: 1,
+  orderBy: "Left Win %",
+  quizMode: false,
+  quizResults: [],
+  selectedGames: [],
+  seenMatchups: {},
+  winsDisplay: [0, 0],
+  lockLeft: false,
 };
 
 const seenMatchupStringify = (newMatchup) => {
@@ -50,38 +51,44 @@ let firstMatchup = randomMatchup(initialState);
 initialState = mutateStateFromNav(initialState, firstMatchup);
 
 const reducer = (prevState = initialState, action) => {
-	switch (action.type) {
-	case "setGameId":
-		return {
-			...prevState,
-			gameId: action.gameId,
-		};
-	case "updateWinsDisplay":
-		return {
-			...prevState,
-			winsDisplay: action.winsDisplay,
-		};
-	case "setMatchup":
-		return mutateStateFromNav(prevState, action.matchup);
-	case "toggleLockLeft":
-		return {
-			...prevState,
-			lockLeft:!(prevState.lockLeft),
-		}
-	  
-    // quiz muts
-	case "pushQuizResult":
-		return {
-			...prevState,
-			quizResults: [...prevState.quizResults, action.result],
-		};
+  switch (action.type) {
+    case "setGameId":
+      return {
+        ...prevState,
+        gameId: action.gameId,
+      };
+    case "updateWinsDisplay":
+      return {
+        ...prevState,
+        winsDisplay: action.winsDisplay,
+      };
+    case "setMatchup":
+      return mutateStateFromNav(prevState, action.matchup);
+    case "toggleLockLeft":
+      return {
+        ...prevState,
+        lockLeft: !prevState.lockLeft,
+      };
 
-	case "toggleQuizMode":
-		return {
-			...prevState,
-			quizMode: action.val,
-			winsDisplay: newWinsDisplay(action.val, prevState.matchup),
-		};
+    // quiz muts
+    case "pushQuizResult":
+      return {
+        ...prevState,
+        quizResults: [...prevState.quizResults, action.result],
+      };
+
+    case "toggleQuizMode":
+      return {
+        ...prevState,
+        quizMode: action.val,
+        winsDisplay: newWinsDisplay(action.val, prevState.matchup),
+      };
+
+    case "setQuizSubmitAnimation":
+      return {
+        ...prevState,
+        displayQuizResults: action.val,
+      };
 
     default:
       return prevState;
