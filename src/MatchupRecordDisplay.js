e019bcd1d03f9273e40bdeb2939b68401cd9eb56
Mart <themcman@gmail.com>
2024-06-09 16:46:51 -0500

    || instead of concat

--- src/MatchupRecordDisplay.js
+++ src/MatchupRecordDisplay.js
@@ -7,11 +7,11 @@ const MatchupRecordDisplay = ({ leftWins, rightWins }) => {
     <div className="matchup-record-display">
       <div className="record-display-percentages">
         <span className="win-text-color">
-          {""+Math.floor((leftWins * 100) / (leftWins + rightWins))}
+          {Math.floor((leftWins * 100) / (leftWins + rightWins))||""}
         </span>
         <span className="percent-divider">:</span>
         <span className="lose-text-color">
-          {""+Math.ceil((rightWins * 100) / (leftWins + rightWins))}
+          {Math.ceil((rightWins * 100) / (leftWins + rightWins))||""}
         </span>
       </div>
     </div>
