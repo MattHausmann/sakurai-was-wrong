5242d8f6b69c376fbbc31a10b26aeb1ebb64476d
Matt Hausmann <matthew.p.hausmann@gmail.com>
2024-06-12 21:07:54 -0500

    whoops didn't add the actual file

---
+++ src/MatchupScoreDisplay.js
@@ -0,0 +1,28 @@
+// src/MatchupScoreDisplay.js
+import React, { useEffect, useRef, useState } from 'react';
+import { useSelector } from "react-redux";
+import './MatchupScoreDisplay.css';
+
+const MatchupScoreDisplay = () => {
+	let { mostRecentScore, bestScore } = useSelector((state) => state.main);	
+
+	const [recentHeight, setRecentHeight] = useState(mostRecentScore * 300 / 10000);
+	const [bestHeight, setBestHeight] = useState(bestScore * 300 / 10000);
+
+	
+	useEffect(() => {
+		setBestHeight(bestScore * 100 / 10000);
+		setRecentHeight(mostRecentScore * 100 / 10000);
+	}, [mostRecentScore, bestScore]);
+
+	return (
+		<div className="score-display-container">
+			<div className="score-bar best-score" style={{ height: `${bestHeight}%` }}>
+			</div>
+			<div className="score-bar recent-score" style={{ height: `${recentHeight}%` }}>
+			</div>
+		</div>
+	);
+};
+
+export default MatchupScoreDisplay;
