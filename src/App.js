387d6085314afc13388db73327a9b1a916bb6cff
Mart <themcman@gmail.com>
2024-06-17 18:01:02 -0500

    reverted rightColumnRef

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import "./App.css";
 const App = () => {
 	const dispatch = useDispatch();
 	const { quizMode, quizResults } = useSelector((state) => state.main);
+	const rightColumnRef = useRef(null);
 	const dialogRef = useRef();
 
 	useEffect(() => {
@@ -24,7 +25,7 @@ const App = () => {
 			});
 		}
 	}, [quizResults]);
-	
+
 	useEffect(()=>{document.title = 'Sakurai Was Wrong'},[]);
 
 	return (
